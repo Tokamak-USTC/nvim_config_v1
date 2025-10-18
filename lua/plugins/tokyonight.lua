0e124a0dd7d3ae9d14f31d0e498a41004876d39c
tokamak <tokamak@mail.ustc.edu.cn>
2025-10-18 19:49:09 +0800

    Add highlight for float border

--- lua/plugins/tokyonight.lua
+++ lua/plugins/tokyonight.lua
@@ -9,38 +9,41 @@ return {
     on_highlights = function(hl, c)
       -- telescope
       hl.TelescopeNormal = { bg = c.bg }
-      hl.TelescopeBorder = { fg = c.border }
-      -- hl.TelescopeBorder = { fg = c.border_highlight }
+      -- hl.TelescopeBorder = { fg = c.border }
+      hl.TelescopeBorder = { fg = c.border_highlight }
       hl.TelescopeTitle = { fg = c.border_highlight }
-      hl.TelescopePromptBorder = { fg = c.border_highlight }
-      hl.TelescopePromptTitle = { fg = c.border_highlight }
+      -- hl.TelescopePromptBorder = { fg = c.border_highlight }
+      -- hl.TelescopePromptTitle = { fg = c.border_highlight }
+      hl.TelescopePromptBorder = { fg = c.yellow }
+      hl.TelescopePromptTitle = { fg = c.yellow }
 
       -- blink-cmp
       hl.BlinkCmpMenu = { bg = c.bg }
-      hl.BlinkCmpMenuBorder = { fg = c.border }
-      -- hl.BlinkCmpMenuBorder = { fg = c.border_highlight }
+      -- hl.BlinkCmpMenuBorder = { fg = c.border }
+      hl.BlinkCmpMenuBorder = { fg = c.border_highlight }
       hl.BlinkCmpDoc = { bg = c.bg }
-      hl.BlinkCmpDocBorder = { fg = c.border }
-      -- hl.BlinkCmpDocBorder = { fg = c.border_highlight }
+      -- hl.BlinkCmpDocBorder = { fg = c.border }
+      hl.BlinkCmpDocBorder = { fg = c.border_highlight }
 
       -- which-key
       hl.WhichKeyNormal = { bg = c.bg }
-      hl.WhichkeyBorder = { fg = c.border }
-      -- hl.WhichkeyBorder = { fg = c.border_highlight }
+      -- hl.WhichkeyBorder = { fg = c.border }
+      hl.WhichkeyBorder = { fg = c.border_highlight }
       hl.WhichKeyTitle = { fg = c.border_highlight }
 
       -- noice
       hl.NoicePopup = { bg = c.bg }
-      hl.NoicePopupBorder = { fg = c.border }
-      -- hl.NoicePopupBorder = { fg = c.border_highlight }
+      -- hl.NoicePopupBorder = { fg = c.border }
+      hl.NoicePopupBorder = { fg = c.border_highlight }
 
       -- snacks
       -- hl.SnacksNormal = { bg = c.bg }
-      hl.SnacksPickerInputBorder = { fg = c.border_highlight }
-      hl.SnacksPickerInputTitle = { fg = c.border_highlight, bg = c.bg }
+      -- hl.SnacksPickerInputBorder = { fg = c.border_highlight }
+      -- hl.SnacksPickerInputTitle = { fg = c.border_highlight, bg = c.bg }
+      hl.SnacksPickerInputBorder = { fg = c.yellow }
+      hl.SnacksPickerInputTitle = { fg = c.yellow, bg = c.bg }
       hl.SnacksPickerBoxTitle = { fg = c.border_highlight, bg = c.bg }
       hl.SnacksPickerInputFooter = { fg = c.border_highlight, bg = c.bg }
-
       -- trouble
       hl.TroubleNormal = { bg = c.bg }
 
@@ -49,8 +52,8 @@ return {
       hl.NormalFloat = { bg = c.bg }
       hl.NormalNC = { bg = c.bg }
       hl.NormalSB = { bg = c.bg }
-      hl.FloatBorder = { fg = c.border }
-      -- hl.FloatBorder = { fg = c.border_highlight }
+      -- hl.FloatBorder = { fg = c.border }
+      hl.FloatBorder = { fg = c.border_highlight }
       hl.FloatTitle = { fg = c.border_highlight }
     end,
   },
